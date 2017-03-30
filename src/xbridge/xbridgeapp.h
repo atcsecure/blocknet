e724544f732f0fe5a010646f9f8a1d99b18bc8f8
corvaxx <maybe.mail@inbox.ru>
2017-03-30 18:52:03 +0300

    rm unused

--- src/xbridge/xbridgeapp.h
+++ src/xbridge/xbridgeapp.h
@@ -22,10 +22,6 @@
 // #include <Ws2tcpip.h>
 #endif
 
-#ifndef NO_GUI
-#include <QApplication>
-#endif
-
 namespace rpc
 {
 class AcceptedConnection;
@@ -192,10 +188,6 @@ public:
 
     static boost::mutex                                  m_ppLocker;
     static std::map<uint256, std::pair<std::string, XBridgePacketPtr> > m_pendingPackets;
-
-//#ifndef NO_GUI
-//    std::shared_ptr<QCoreApplication> m_app;
-//#endif
 };
 
 #endif // XBRIDGEAPP_H
