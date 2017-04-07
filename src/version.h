5772dd6b082bf652caa36190f2eb716a1ecdcf51
Dan Metcalf <dmetcalf@hsdp.io>
2017-04-07 12:04:22 -0400

    version bump and dx enabled

--- src/version.h
+++ src/version.h
@@ -13,9 +13,9 @@
 
 static const int CLIENT_VERSION =
                            1000000 * CLIENT_VERSION_MAJOR
-                         +   20000 * CLIENT_VERSION_MINOR
-                         +     200 * CLIENT_VERSION_REVISION
-                         +       6 * CLIENT_VERSION_BUILD;
+                         +   10000 * CLIENT_VERSION_MINOR
+                         +     100 * CLIENT_VERSION_REVISION
+                         +       1 * CLIENT_VERSION_BUILD;
 
 extern const std::string CLIENT_NAME;
 extern const std::string CLIENT_BUILD;
