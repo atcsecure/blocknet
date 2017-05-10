501f37b5ff3e54b57cf2a74cea9ff1a137aa122c
corvaxx <maybe.mail@inbox.ru>
2017-05-10 12:54:41 +0300

    up version

--- src/clientversion.h
+++ src/clientversion.h
@@ -9,7 +9,7 @@
 #define CLIENT_VERSION_MAJOR       1
 #define CLIENT_VERSION_MINOR       4
 #define CLIENT_VERSION_REVISION    4
-#define CLIENT_VERSION_BUILD      19
+#define CLIENT_VERSION_BUILD      20
 
 // Converts the parameter X to a string after macro replacement on X has been performed.
 // Don't merge these into one macro!
